6be5d4f1a29f26beb1af7c58d7dc6675e4a24631
vapenzin <chyaldon@gmail.com>
2016-10-02 19:31:00 +0300

    method attach was implemented in abstract class

--- Branch.java
+++ Branch.java
@@ -1,9 +1,9 @@
  import java.util.*;
 
 public class Branch extends Implementation {
-  public Branch(String branchName) {
-    this.setName(branchName);
-    System.out.println("A new Branch " + this.getName());
-	};
+ 	 public Branch(String branchName) {
+ 		 this.setName(branchName);
+ 		 System.out.println("A new Branch " + this.getName());
+ 	 };
 }
 
