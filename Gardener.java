c54db244f3b3332aab626ccfd0e33ca6a35f1255
vapenzin <chyaldon@gmail.com>
2016-10-03 20:03:01 +0300

    Plants as time of objects of different classes extends by Plants

--- Gardener.java
+++ Gardener.java
@@ -1,15 +1,15 @@
 
 public class Gardener {
 	public static void main(String[] args) {
-		  Trunk tr1 = new Trunk("tr1");
+		  Plants tr1 = new Trunk("tr1");
 		  tr1.setName("boba");
-		  Trunk tr2 = new Trunk("tr2");
-		  Branch br1 = new Branch("br1");
-		  Branch br2 = new Branch("br2");
+		  Plants tr2 = new Trunk("tr2");
+		  Plants br1 = new Branch("br1");
+		  Plants br2 = new Branch("br2");
 		  tr1.attach(br1);
 		  br1.attach(br2);
-		  Leaf lf1 = new Leaf("lf1");
-		  Leaf lf2 = new Leaf("lf2");
+		  Plants lf1 = new Leaf("lf1");
+		  Plants lf2 = new Leaf("lf2");
 		  br2.attach(lf1);
 		  System.out.println(tr1.getName());
 		  lf1.attach(lf2);
