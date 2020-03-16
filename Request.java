e4843b350356c8b5df381e646e098923397d5fd9
Vladimir Penzin <vlpe1118@netcracker.com>
2020-03-16 23:39:39 +0300

    pull request try

---
+++ Request.java
@@ -0,0 +1,17 @@
+
+
+public class Request {
+    private String name;
+
+    public Request(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
